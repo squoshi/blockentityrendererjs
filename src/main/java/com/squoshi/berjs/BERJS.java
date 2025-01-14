dff2718f3226921dd452dedc51a793110f71ed7c
squoshi <95097230+squoshi@users.noreply.github.com>
2025-01-13 23:17:39 -0600

    jank

--- src/main/java/com/squoshi/berjs/BERJS.java
+++ src/main/java/com/squoshi/berjs/BERJS.java
@@ -1,22 +1,48 @@
 package com.squoshi.berjs;
 
+import com.squoshi.berjs.event.RegisterBlockEntityRenderersEventJS;
 import dev.latvian.mods.rhino.util.HideFromJS;
-import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
+import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 import java.util.HashMap;
 
 @Mod(BERJS.MODID)
+@Mod.EventBusSubscriber(modid = BERJS.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class BERJS {
     public static final String MODID = "berjs";
 
-    private static final HashMap<BlockEntityType<?>, BlockEntityRenderer<?>> blockEntityRenderers = new HashMap<>();
+    private static final HashMap<BlockEntityType<?>, BlockEntityRendererProvider<?>> blockEntityRenderers = new HashMap<>();
 
     public BERJS() {}
 
     @HideFromJS
-    public static void addRenderer(BlockEntityType<?> type, BlockEntityRenderer<?> renderer) {
-        blockEntityRenderers.put(type, renderer);
+    public static void addRenderer(BlockEntityType<?> type, BlockEntityRendererProvider<?> provider) {
+        blockEntityRenderers.put(type, provider);
+    }
+
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        RegisterBlockEntityRenderersEventJS e = new RegisterBlockEntityRenderersEventJS();
+        if (BERJSPlugin.registerBlockEntityRenderers.hasListeners()) {
+            BERJSPlugin.registerBlockEntityRenderers.post(e);
+        }
+    }
+
+    @Mod.EventBusSubscriber(modid = BERJS.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class BERJSClient {
+        @SubscribeEvent
+        public static void registerBlockEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
+            blockEntityRenderers.forEach((type, provider) -> registerRenderer(event, type, provider));
+        }
+
+        private static <T extends BlockEntity> void registerRenderer(EntityRenderersEvent.RegisterRenderers event, BlockEntityType<?> type, BlockEntityRendererProvider<?> provider) {
+            event.registerBlockEntityRenderer((BlockEntityType<T>) type, (BlockEntityRendererProvider<? super T>) provider);
+        }
     }
 }
--- src/main/java/com/squoshi/berjs/event/RegisterBlockEntityRenderersEventJS.java
+++ src/main/java/com/squoshi/berjs/event/RegisterBlockEntityRenderersEventJS.java
@@ -1,6 +1,21 @@
 package com.squoshi.berjs.event;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.squoshi.berjs.BERJS;
 import dev.latvian.mods.kubejs.event.EventJS;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 
+import java.util.function.Consumer;
+
+@SuppressWarnings("unused")
 public class RegisterBlockEntityRenderersEventJS extends EventJS {
+    public static void registerBlockEntityRenderer(BlockEntityType<?> type, Consumer<BlockEntityRendererContext> renderer) {
+        BlockEntityRendererProvider<BlockEntity> provider = context -> (entity, partialTicks, poseStack, bufferSource, light, overlay) -> renderer.accept(new BlockEntityRendererContext(entity, partialTicks, poseStack, bufferSource, light, overlay));
+        BERJS.addRenderer(type, provider);
+    }
+
+    public record BlockEntityRendererContext(BlockEntity entity, float partialTicks, PoseStack poseStack, MultiBufferSource bufferSource, int light, int overlay) { }
 }
